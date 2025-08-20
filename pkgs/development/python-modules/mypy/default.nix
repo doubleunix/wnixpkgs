b6a0e0b02ec0d53d9c098a29bf66679d1b1615b4
Jason Wilkes <notarealdeveloper@gmail.com>
2025-08-20 12:41:52 -0500

    Prevent mypy from failing on 3.15

--- pkgs/development/python-modules/mypy/default.nix
+++ pkgs/development/python-modules/mypy/default.nix
@@ -66,6 +66,12 @@ buildPythonPackage rec {
   ]
   ++ lib.optionals (pythonOlder "3.11") [ tomli ];
 
+  # Relax -Werror for deprecation warnings on 3.15+
+  NIX_CFLAGS_COMPILE =
+    lib.optionals (lib.versionAtLeast python.pythonVersion "3.15") [
+      "-Wno-error=deprecated-declarations"
+    ];
+
   optional-dependencies = {
     dmypy = [ psutil ];
     reports = [ lxml ];
